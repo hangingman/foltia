988b9f06f0c6f83d0a89710d425558b38cdd4e23
sorshi <sorshi@811c5640-5c23-0410-a611-fd7ef4895f6f>
2006-12-18 08:45:28 +0000

    録画ライブラリ、サブタイトルへのリンク追加、ページタイトルに番組名表示。
    git-svn-id: http://svn.dcc-jpl.com/foltia/trunk@23 811c5640-5c23-0410-a611-fd7ef4895f6f

--- install/php/showlibc.php
+++ install/php/showlibc.php
@@ -33,29 +33,16 @@ if ($tid == "") {
 <script src="http://images.apple.com/main/js/ac_quicktime.js" language="JavaScript" type="text/javascript"></script>
 <?php
 print "<link rel=\"alternate\" type=\"application/rss+xml\" title=\"RSS\" href=\"./folcast.php?tid=$tid\" />
-<title>foltia:Lib $tid</title>
-</head>
 ";
 		if ($tid == "") {
-	print "<body BGCOLOR=\"#ffffff\" TEXT=\"#494949\" LINK=\"#0047ff\" VLINK=\"#000000\" ALINK=\"#c6edff\" > \n";
+	print "<title>foltia:Lib</title>
+</head><body BGCOLOR=\"#ffffff\" TEXT=\"#494949\" LINK=\"#0047ff\" VLINK=\"#000000\" ALINK=\"#c6edff\" > \n";
 		printhtmlpageheader();
 		die_exit("再生可能番組がありません<BR>");
 		}
-
 $con = m_connect();
 $now = date("YmdHi");   
 
-?>
-<body BGCOLOR="#ffffff" TEXT="#494949" LINK="#0047ff" VLINK="#000000" ALINK="#c6edff" >
-<div align="center">
-<?php 
-	printhtmlpageheader();
-?>
-  <p align="left"><font color="#494949" size="6">録画ライブラリ番組個別表示</font></p>
-  <hr size="4">
-<p align="left">再生可能ムービーを表示します。<br>
-
-<?php
 $query = "
 SELECT foltia_program.title  
 FROM  foltia_program   
@@ -69,15 +56,31 @@ $syobocaldb = mb_convert_encoding($syobocaldb, "EUC-JP", "UTF-8");
 	$syobocaldb = preg_match("/<Title>.*<\/Title>/", $syobocaldb,$title);
 	$title = $title[0];
 	$title = strip_tags($title);
+	$title =  htmlspecialchars($title) ;
 }else{
 $rowdata = pg_fetch_row($rs, 0);
 $title = $rowdata[0];
+$title =  htmlspecialchars($title) ;
 }
+//ヘッダ続き
+print "<title>foltia:Lib $tid:$title</title>
+</head>
+<body BGCOLOR=\"#ffffff\" TEXT=\"#494949\" LINK=\"#0047ff\" VLINK=\"#000000\" ALINK=\"#c6edff\" >
+<div align=\"center\">
+";
+	printhtmlpageheader();
+print "  <p align=\"left\"><font color=\"#494949\" size=\"6\">録画ライブラリ番組個別表示</font></p>
+  <hr size=\"4\">
+<p align=\"left\">再生可能ムービーを表示します。<br>";
+
+
+if ($tid == 0){
+print "$title 【<A HREF = \"./folcast.php?tid=$tid\">この番組のFolcast</A>】 <br>\n";
+}else{
+
 print "<a href=\"http://cal.syoboi.jp/tid/" .
-				     htmlspecialchars($tid)  . "\" target=\"_blank\">" .
-				     htmlspecialchars($title) . "</a> 【<A HREF = \"./folcast.php?tid=$tid\">この番組のFolcast</A>】 <br>\n";
-					 
-					 
+				     htmlspecialchars($tid)  . "\" target=\"_blank\">$title</a> 【<A HREF = \"./folcast.php?tid=$tid\">この番組のFolcast</A>】 <br>\n";
+}
 //確認
 if (file_exists ("$recfolderpath/$tid.localized")){
 //	print "ディレクトリは存在します\n";
@@ -145,6 +148,7 @@ $title = htmlspecialchars($title);
 $count = htmlspecialchars($count);
 $subtitle = htmlspecialchars($subtitle);
 $onairdate = htmlspecialchars($onairdate);
+$pid = htmlspecialchars($rowdata[6]);
 $fName = htmlspecialchars($rowdata[7]);
 if (ereg(".MP4", $fName)){
 	$thumbnail = $fName;
@@ -172,141 +176,30 @@ if (($sbpluginexist == 1) && (pg_num_rows ($rs ) > 0)){
 $caplink = "";
 }//end if sb
 
-print "
-  <tr>
-    <td rowspan=\"4\" width=\"170\"><A HREF=\"$httpmediamappath/$tid.localized/mp4/$fName\" target=\"_blank\"><IMG SRC = \"$httpmediamappath/$tid.localized/mp4/$thumbnail\" WIDTH = \"160\" HEIGHT = \"120\"></A></td>
+print "  <tr>
+    <td rowspan=\"4\" width=\"170\"><a href = \"$httpmediamappath/$tid.localized/mp4/$fName\" target=\"_blank\"><img src = \"$httpmediamappath/$tid.localized/mp4/$thumbnail\" width = \"160\" height = \"120\"></A></td>
     <td>$count</td>
   </tr>
   <tr>
-    <td>$subtitle</td>
-  </tr>
+";
+if ($tid == 0){
+print "\n    <td>$subtitle</td>";
+}else{
+print "\n    <td><a href = \"http://cal.syoboi.jp/tid/$tid/time#$pid\" target=\"_blank\">$subtitle</a></td>";
+}//if
+print "  </tr>
   <tr>
     <td>$onairdate</td>
   </tr>
   <tr>
-    <td><A HREF=\"$httpmediamappath/$tid.localized/mp4/$fName\" target=\"_blank\">$fName</A> / <script language=\"JavaScript\" type=\"text/javascript\">QT_WriteOBJECT_XHTML('http://g.hatena.ne.jp/images/podcasting.gif','16','16','','controller','FALSE','href','http://$serverfqdn/$httpmediamappath/$tid.localized/mp4/$fName','target','QuickTimePlayer','type','video/mp4');</script> $caplink</td>
+    <td><a href =\"$httpmediamappath/$tid.localized/mp4/$fName\" target=\"_blank\">$fName</A> / <script language=\"JavaScript\" type=\"text/javascript\">QT_WriteOBJECT_XHTML('http://g.hatena.ne.jp/images/podcasting.gif','16','16','','controller','FALSE','href','http://$serverfqdn/$httpmediamappath/$tid.localized/mp4/$fName','target','QuickTimePlayer','type','video/mp4');</script> $caplink</td>
   </tr>
 ";
-
 
 }//for
 }else{
 print "録画ファイルがありません<br>\n";
 }//if
-
-//旧仕様
-/*
-//ディレクトリからファイル一覧を取得
-	exec ("ls $recfolderpath/$tid.localized/mp4/*.MP4", $mp4files);
-
-if (file_exists("./selectcaptureimage.php") ) {
-	$sbpluginexist = 1;
-}
-
-$serverfqdn = getserverfqdn();
-
-foreach($mp4files as $pathfName) {
-$fName = "";
-$fNametmp = split("/",$pathfName);
-$fName = array_pop($fNametmp);
- if(($fName == ".") or ($fName == "..") ){ continue; }
-
-if (ereg(".MP4", $fName)){
-	$thumbnail = $fName;
-	$thumbnail = ereg_replace(".MP4", ".THM", $thumbnail);
-	//	$filesplit = split("-",$fName);
-//print "<!-- FILENAME:$fName:$pathfName:$query -->\n";
-$query = "";
-
-$query = "
-SELECT 
-foltia_program.tid,
-foltia_program.title,
-foltia_subtitle.countno,
-foltia_subtitle.subtitle,
-foltia_subtitle.startdatetime ,
-foltia_subtitle.m2pfilename ,
-foltia_subtitle.pid  
-FROM foltia_subtitle , foltia_program   
-WHERE foltia_subtitle.pspfilename = '$fName' 
-AND foltia_program.tid = foltia_subtitle.tid 
-AND foltia_program.tid = $tid 
-";
-//print "<!-- FILENAME:$fName:$pathfName:$query -->\n";
-$rs = "";
-$rs = m_query($con, $query, "DBクエリに失敗しました");
-
-if (pg_num_rows ($rs )  == 0){
-
-	$count = "[話数]";
-	$subtitle = "[サブタイトル]";
-	$onairdate = "[放映日]";
-	$caplink = "";
-}else{
-
-$rowdata = pg_fetch_row($rs, $row);
-$title = $rowdata[1];
-
-if ($rowdata[2]== "" ){
-	$count = "[話数]";
-}else{
-	$count = $rowdata[2];
-}
-if ($rowdata[3]== "" ){
-	$subtitle = "[サブタイトル]";
-}else{
-	$subtitle = $rowdata[3];
-}
-$onairdate =  $rowdata[4];
-
-$tid = htmlspecialchars($rowdata[0]);
-$title = htmlspecialchars($title);
-$count = htmlspecialchars($count);
-$subtitle = htmlspecialchars($subtitle);
-$onairdate = htmlspecialchars($onairdate);
-
-$day = substr($onairdate,0,4)."/".substr($onairdate,4,2)."/".substr($onairdate,6,2);
-$time = substr($onairdate,8,2).":".substr($onairdate,10,2);
-$onairdate = "$day $time";
-
-//Starlight Breaker向け拡張
-//$debug_pg_num_rows = pg_num_rows ($rs );
-$caplink = "";
-
-if (($sbpluginexist == 1) && (pg_num_rows ($rs ) > 0)){
- $capimgpath = htmlspecialchars(preg_replace("/.m2p/", "", $rowdata[5]));
-	if (file_exists("$recfolderpath/$tid.localized/img/$capimgpath") ){
-	$caplink = " / <a href = \"./selectcaptureimage.php?pid=$rowdata[6]\">キャプ</a>";
-	}else{
-	$caplink = " / キャプなし";
-	}
-}else{
-$caplink = "";
-}//end if sb
-
-} //end if 検索件数が0のとき
-//--
-print "
-  <tr>
-    <td rowspan=\"4\" width=\"170\"><A HREF=\"$httpmediamappath/$tid.localized/mp4/$fName\" target=\"_blank\"><IMG SRC = \"$httpmediamappath/$tid.localized/mp4/$thumbnail\" WIDTH = \"160\" HEIGHT = \"120\"></A></td>
-    <td>$count</td>
-  </tr>
-  <tr>
-    <td>$subtitle</td>
-  </tr>
-  <tr>
-    <td>$onairdate</td>
-  </tr>
-  <tr>
-    <td><A HREF=\"$httpmediamappath/$tid.localized/mp4/$fName\" target=\"_blank\">$fName</A> / <script language=\"JavaScript\" type=\"text/javascript\">QT_WriteOBJECT_XHTML('http://g.hatena.ne.jp/images/podcasting.gif','16','16','','controller','FALSE','href','http://$serverfqdn/$httpmediamappath/$tid.localized/mp4/$fName','target','QuickTimePlayer','type','video/mp4');</script> $caplink</td>
-  </tr>
-";
-
-	}//end if ereg m2p
-}//end foreach
-
-*/
-//旧仕様ココまで
 ?>
 	</tbody>
 </table>
