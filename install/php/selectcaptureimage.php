453107f6f0609dbe04193423bb8d7de734d59916
sorshi <sorshi@811c5640-5c23-0410-a611-fd7ef4895f6f>
2007-01-08 15:25:25 +0000

    ショボかるへのリンクを修正。
    git-svn-id: http://svn.dcc-jpl.com/foltia/trunk@37 811c5640-5c23-0410-a611-fd7ef4895f6f

--- install/php/selectcaptureimage.php
+++ install/php/selectcaptureimage.php
@@ -18,8 +18,6 @@ mplayer -ss 00:00:10 -vo jpeg:outdir=/home/foltia/php/tv/691.localized/img/6/ -v
 
 include("./foltialib.php");
 
-//$tid = getgetnumform(tid);
-//$path = getgetform(d);
 $pid = getgetform(pid);
 
 if ($pid == "") {
@@ -72,7 +70,7 @@ WHERE foltia_program.tid = foltia_subtitle.tid AND foltia_station.stationid = fo
 print "  <p align=\"left\"><font color=\"#494949\" size=\"6\">キャプチャ画像</font></p>
   <hr size=\"4\">
 <p align=\"left\">";
-print "<a href = \"http://cal.syoboi.jp/tid/$rowdata[0]/time#$pid\" target=\"_blank\">";
+print "<a href = \"http://cal.syoboi.jp/tid/$rowdata[0]/\" target=\"_blank\">";
 print htmlspecialchars($rowdata[2]) . "</a> " ;
 print htmlspecialchars($rowdata[3]) . " ";
 $tid = $rowdata[0];
@@ -95,13 +93,7 @@ $m2pfilename = $rowdata[8];
 
 list($tid,$countno,$date,$time)= split ("-", $m2pfilename );
 	$tid = ereg_replace("[^0-9]", "", $tid);
-//if ($countno == "x"){
-//}else{
-//	$countno = ereg_replace("[^0-9]", "", $countno);
-//}
-//	$date = ereg_replace("[^0-9]", "", $date);
-//	$time = ereg_replace("[^0-9]", "", $time);
-//$path = $tid."-".$countno."-".$date."-".$time ;
+
 $path = ereg_replace("\.m2p$", "", $m2pfilename);
 $serveruri = getserverfqdn ();
 
@@ -109,14 +101,7 @@ exec ("ls   $recfolderpath/$tid.localized/img/$path/", $tids);
 //$timecount = 1;
 foreach($tids as $filetid) {
 print "<IMG SRC='http://$serveruri$httpmediamappath/$tid.localized/img/$path/$filetid' WIDTH='160' HEIGHT='120'  ALT='$tid:$countno:$filetid'>\n";
-/*
-$i++ ;
-if ($i > 3){
- print "<br>\n";
-$i = 0;
-//$timecount++ ;
-}
-*/
+
 }//foreach
 // タイトル一覧　ここまで
 
